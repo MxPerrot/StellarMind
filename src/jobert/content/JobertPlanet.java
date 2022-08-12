fb15ac09e9ace3cf3416115c43e75fc4c9e47bec
Jojo <110781915+JojoFR1@users.noreply.github.com>
2022-08-12 17:42:18 +0200

    Update JobertPlanet.java

--- src/jobert/content/JobertPlanet.java
+++ src/jobert/content/JobertPlanet.java
@@ -15,16 +15,16 @@ public class JobertPlanet implements ContentList {
 
     public void load() {
         testing = new Planet("testing", Planets.sun, 1f, 3) {{
-        localizedName = "Testing Planet";
-        generator = new ErekirPlanetGenerator;
-        startSector = 1;
-        hasAtmosphere = true;
-        atmosphereColor = Color.valueOf("8c3149");
-        atmosphereRadIn = 0.01f;
-        atmosphereRadOut = 0.4f;
-        meshLoader = () -> new HexMesh(this, 5);
-        orbitRadius = 4f;
-        alwaysUnlocked = true;
+            localizedName = "Testing Planet";
+            generator = new ErekirPlanetGenerator;
+            startSector = 1;
+            hasAtmosphere = true;
+            atmosphereColor = Color.valueOf("8c3149");
+            atmosphereRadIn = 0.01f;
+            atmosphereRadOut = 0.4f;
+            meshLoader = () -> new HexMesh(this, 5);
+            orbitRadius = 4f;
+            alwaysUnlocked = true;
         }};
     }
 }
