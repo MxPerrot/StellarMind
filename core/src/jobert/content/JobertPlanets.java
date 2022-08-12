e2f8edc2f811eea75af2105ee53fd8142c284665
Jojo <110781915+JojoFR1@users.noreply.github.com>
2022-08-12 21:24:23 +0200

    Fixed missing content from Mindustry

--- core/src/jobert/content/JobertPlanets.java
+++ core/src/jobert/content/JobertPlanets.java
@@ -14,6 +14,7 @@ import mindustry.maps.planet.*;
 import mindustry.type.*;
 import mindustry.world.*;
 import mindustry.world.meta.*;
+import mindustry.content.*
 
 public class JobertPlanets{
     public static Planet testing;
