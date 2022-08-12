9d46bd74230e0e16d89a10f528bfc963f487b38f
Jojo <110781915+JojoFR1@users.noreply.github.com>
2022-08-12 22:10:27 +0200

    Update JoBertMod.java

--- core/src/jobert/JoBertMod.java
+++ core/src/jobert/JoBertMod.java
@@ -1,36 +1,36 @@
 package jobert;
 
+import acontent.ui.*;
 import arc.*;
+import arc.audio.*;
+import arc.func.*;
+import arc.math.*;
+import arc.struct.*;
 import arc.util.*;
+import arc.graphics.*;
 import mindustry.*;
 import mindustry.content.*;
+import mindustry.entities.*;
+import mindustry.entities.bullet.*;
 import mindustry.game.EventType.*;
 import mindustry.gen.*;
 import mindustry.mod.*;
-import mindustry.ui.dialogs.*;
+import mindustry.mod.Mods.*;
+import mindustry.type.*;
+import mindustry.input.*;
+// Import fichier mod
 import jobert.content.*;
 
 public class JoBertMod extends Mod{
 
     public JoBertMod(){
-        Log.info("Loaded ExampleJavaMod constructor.");
-
-        //listen for game load event
         Events.on(ClientLoadEvent.class, e -> {
-            //show dialog upon startup
-            Time.runTask(10f, () -> {
-                BaseDialog dialog = new BaseDialog("Welcome!");
-                dialog.cont.add("Je garde ce message car j'y connais rien ! Cordialement, Da Jojo.").row();
-                //mod sprites are prefixed with the mod name (this mod is called 'example-java-mod' in its config)
-                dialog.cont.button("I see", dialog::hide).size(100f, 50f);
-                dialog.show();
-            });
+            JoBertPlanets.load;
         });
-    }
 
     @Override
     public void loadContent(){
-        Log.info("Loading some example content.");
+        Log.info("JoBert Industries successfully loaded!");
     }
 
 }
