1b19926eba5712b9b3f669fd1e009ae14737d245
Jojo <110781915+JojoFR1@users.noreply.github.com>
2022-08-12 13:26:41 +0200

    Update JoBertMod.java

--- core/src/jobert/JoBertMod.java
+++ core/src/jobert/JoBertMod.java
@@ -9,9 +9,9 @@ import mindustry.gen.*;
 import mindustry.mod.*;
 import mindustry.ui.dialogs.*;
 
-public class ExampleJavaMod extends Mod{
+public class JoBertMod extends Mod{
 
-    public ExampleJavaMod(){
+    public JoBertMod{
         Log.info("Loaded ExampleJavaMod constructor.");
 
         //listen for game load event
