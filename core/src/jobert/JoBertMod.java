a04be14387b9a87214ebb82bd52d7e529fabc173
Jojo <110781915+JojoFR1@users.noreply.github.com>
2022-08-13 02:24:49 +0200

    Update JoBertMod.java

--- core/src/jobert/JoBertMod.java
+++ core/src/jobert/JoBertMod.java
@@ -1,30 +1,12 @@
 package jobert;
 
-import arc.*;
-import arc.audio.*;
-import arc.func.*;
-import arc.math.*;
-import arc.struct.*;
-import arc.util.*;
-import arc.graphics.*;
-import mindustry.*;
-import mindustry.content.*;
-import mindustry.entities.*;
-import mindustry.entities.bullet.*;
-import mindustry.game.EventType.*;
-import mindustry.gen.*;
 import mindustry.mod.*;
-import static mindustry.mod.Mods.*;
-import mindustry.type.*;
-import mindustry.input.*;
-
-import static arc.Core.*;
-import static mindustry.Vars.*;
-// Import fichier mod
 import jobert.content.*;
 
 public class JoBertMod extends Mod{
+
     public JoBertMod (){
+
     }
     @Override
     public void loadContent(){
