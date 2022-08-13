ebef93a9a6e4680b1d4cb363139cdc02b76316c7
Jojo <110781915+JojoFR1@users.noreply.github.com>
2022-08-13 03:47:24 +0200

    Fix HexMesh?

--- core/src/jobert/content/JBPlanets.java
+++ core/src/jobert/content/JBPlanets.java
@@ -25,7 +25,7 @@ public class JBPlanets {
             atmosphereRadIn = 0.01f;
             atmosphereRadOut = 0.4f;
             // Mesh
-            meshLoader = () -> new HexMesh(this, 6);
+            meshLoader = () -> new HexMesh(this, 7);
             cloudMeshLoader = () -> new MultiMesh(
                       new HexSkyMesh(this, 11, 0.15f, 0.13f, 5, new Color().set(Color.valueOf("363f9a")).mul(0.9f).a(0.75f), 2, 0.45f, 0.9f, 0.38f),
                       new HexSkyMesh(this, 1, 0.6f, 0.16f, 5, Color.white.cpy().lerp(Color.valueOf("363f9a"), 0.55f).a(0.75f), 2, 0.45f, 1f, 0.41f)
