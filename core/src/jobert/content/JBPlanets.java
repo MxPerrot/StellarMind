006ab549bdaa6a9f412e50feecc5b1c48ca65a52
Jojo <110781915+JojoFR1@users.noreply.github.com>
2022-08-13 05:06:09 +0200

    Add generator, possible Mesh fix

--- core/src/jobert/content/JBPlanets.java
+++ core/src/jobert/content/JBPlanets.java
@@ -19,13 +19,14 @@ public class JBPlanets {
             localizedName = "Testing Planet";
             description = "A testing planet.";
             landCloudColor = atmosphereColor.cpy().a(0.5f);
+            generator = new ErekirPlanetGenerator();
             // Atmosphère
             hasAtmosphere = true;
             atmosphereColor = Color.valueOf("363f9a");
             atmosphereRadIn = 0.01f;
             atmosphereRadOut = 0.4f;
             // Mesh
-            //meshLoader = () -> new HexMesh(this, 1);
+            meshLoader = () -> new HexMesh(this, 6);
             cloudMeshLoader = () -> new MultiMesh(
                       new HexSkyMesh(this, 11, 0.15f, 0.13f, 5, new Color().set(Color.valueOf("363f9a")).mul(0.9f).a(0.75f), 2, 0.45f, 0.9f, 0.38f),
                       new HexSkyMesh(this, 1, 0.6f, 0.16f, 5, Color.white.cpy().lerp(Color.valueOf("363f9a"), 0.55f).a(0.75f), 2, 0.45f, 1f, 0.41f)
