e68f2643b1893fbcb3e3a1156d1ca33d254b7480
Jojo <110781915+JojoFR1@users.noreply.github.com>
2022-08-13 19:11:53 +0200

    Create JBBlock.java

---
+++ core/src/jobert/content/Blocks/JBBlock.java
@@ -0,0 +1,37 @@
+package jobert.content.Blocks;
+
+import mindustry.content.Items;
+import mindustry.ctype.ContentList;
+import mindustry.type.Category;
+import mindustry.type.ItemStack;
+import mindustry.world.Block;
+import mindustry.world.meta.BuildVisibility;
+
+import static mindustry.type.ItemStack.with;
+
+public class JBBlocks implements ContentList {
+    public static Block
+
+        //Environment
+        
+        //Transportation
+        
+        //Power
+        
+        //Production
+        
+        //Units
+        
+        //Turrets
+        
+        //Walls
+        
+        private ContentList[] blocksContent = {
+                new JBProduction(),
+        };
+
+        public void load() {
+            for (ContentList contentList : blocksContent) {
+                contentList.load();
+        }
+}
