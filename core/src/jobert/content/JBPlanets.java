b6097ce3adaa045766afb4acec9e018a80b6348b
Jojo <110781915+JojoFR1@users.noreply.github.com>
2022-08-13 17:14:37 +0200

    Update JBPlanets.java

--- core/src/jobert/content/JBPlanets.java
+++ core/src/jobert/content/JBPlanets.java
@@ -39,7 +39,7 @@ public class JBPlanets {
             bloom = true;
         }};
 
-        arkans = new Planet("arkans", elios, 1.5f, 3) {{
+        arkans = new Planet("arkans", elios, 10f, 2) {{
             localizedName = "Arkans";
             description = "A testing planet.";
             landCloudColor = atmosphereColor.cpy().a(0.5f);
