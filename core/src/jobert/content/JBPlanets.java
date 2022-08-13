96870156a8e9eba1c9b5074ef22e0821ea07f250
Jojo <110781915+JojoFR1@users.noreply.github.com>
2022-08-13 03:25:33 +0200

    Essaie de fix les erreurs

--- core/src/jobert/content/JBPlanets.java
+++ core/src/jobert/content/JBPlanets.java
@@ -19,15 +19,19 @@ public class JBPlanets {
         testing = new Planet("testing", Planets.sun, 1f, 3) {{
             localizedName = "Testing Planet";
             description = "A testing planet.";
+            landCloudColor = atmosphereColor.cpy().a(0.5f);
             // Atmosphère
             hasAtmosphere = true;
-            atmosphereColor = Color.valueOf("8c3149");
+            atmosphereColor = Color.valueOf("363f9a");
             atmosphereRadIn = 0.01f;
             atmosphereRadOut = 0.4f;
-            // Orbite
-            orbitRadius = 2f;
-            //Mesh
+            // Mesh
             meshLoader = () -> new HexMesh(this, 6);
+            cloudMeshLoader = () -> new MultiMesh(
+                      new HexSkyMesh(this, 11, 0.15f, 0.13f, 5, new Color().set(Color.valueOf("363f9a")).mul(0.9f).a(0.75f), 2, 0.45f, 0.9f, 0.38f),
+                      new HexSkyMesh(this, 1, 0.6f, 0.16f, 5, Color.white.cpy().lerp(Color.valueOf("363f9a"), 0.55f).a(0.75f), 2, 0.45f, 1f, 0.41f)
+            // Orbite 
+            orbitRadius = 2f;
 
             alwaysUnlocked = true;
         }};
