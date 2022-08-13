84956e16d2ec02bc0f03397aa6c885aa9b690dd1
Jojo <110781915+JojoFR1@users.noreply.github.com>
2022-08-13 03:27:44 +0200

    Little fixy dixy :)

--- core/src/jobert/content/JBPlanets.java
+++ core/src/jobert/content/JBPlanets.java
@@ -30,6 +30,7 @@ public class JBPlanets {
             cloudMeshLoader = () -> new MultiMesh(
                       new HexSkyMesh(this, 11, 0.15f, 0.13f, 5, new Color().set(Color.valueOf("363f9a")).mul(0.9f).a(0.75f), 2, 0.45f, 0.9f, 0.38f),
                       new HexSkyMesh(this, 1, 0.6f, 0.16f, 5, Color.white.cpy().lerp(Color.valueOf("363f9a"), 0.55f).a(0.75f), 2, 0.45f, 1f, 0.41f)
+                              );
             // Orbite 
             orbitRadius = 2f;
 
