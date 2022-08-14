e81850ea38f499f935e5b514a9b11399311deba2
Jojo <110781915+JojoFR1@users.noreply.github.com>
2022-08-14 04:16:53 +0200

    Update JBBlocks.java

--- core/src/jobert/content/JBBlocks.java
+++ core/src/jobert/content/JBBlocks.java
@@ -87,7 +87,7 @@ public class JBBlocks {
             craftTime = 60;
             consumePower(2.33333333f);
             consumesLiquid(new LiquidStack(Liquids.water, 0.3f));
-            consumesItem(new ItemStack(Items.titanium, 3));
+            consumesItem[ItemStack[Items.titanium, 3]];
             outputLiquid = new LiquidStack(Liquids.cryofluid, 0.366666667f);
             buildVisibility = sandboxOnly;
         }};
