c4f7d6c8cb4757c5c61c5a10fe7b5cafdf614ee4
Jojo <110781915+JojoFR1@users.noreply.github.com>
2022-08-20 01:47:36 +0200

    Update ArkansPlanetGenerator.java

--- core/src/stellar/maps/planet/ArkansPlanetGenerator.java
+++ core/src/stellar/maps/planet/ArkansPlanetGenerator.java
@@ -26,26 +26,6 @@ import stellar.content.SBlocks;
 public class ArkansPlanetGenerator extends PlanetGenerator {
     public float heightScl = 0.9f, octaves = 8, persistence = 0.7f, heightPow = 3f, heightMult = 1.6f;
 
-    Block[] terrain = {SBlocks.floorArkans};
-
-    {
-        baseSeed = 3;
-    }
-
-    @Override
-    public float getHeight(Vec3 position){
-        return Mathf.pow(rawHeight(position), heightPow) * heightMult;
-    }
-
-    @Override
-    public Color getColor(Vec3 position){
-        Block block = getBlock(position);
-
-        if(block == SBlocks.floorArkans) block = SBlocks.floorArkans;
-
-        return Tmp.c1.set(block.mapColor).a (1f);
-    }
-
     float rawHeight(Vec3 position){
         return Simplex.noise3d(seed, octaves, persistence, 1f/heightScl, 10f + position.x, 10f + position.y, 10f + position.z);
     }
@@ -66,5 +46,75 @@ public class ArkansPlanetGenerator extends PlanetGenerator {
         Block result = terrain[Mathf.clamp((int)(height * terrain.length), 0, terrain.length - 1)];
 
         return result;
+
+
+    Block[] terrain = {SBlocks.floorArkans};
+
+    {
+        baseSeed = 3;
     }
+
+    @Override
+    public float getHeight(Vec3 position){
+        return Mathf.pow(rawHeight(position), heightPow) * heightMult;
+    }
+
+    @Override
+    public Color getColor(Vec3 position){
+        Block block = getBlock(position);
+        tnoise = 1f;
+
+        if(block == SBlocks.floorArkans) block = SBlocks.floorArkans;
+
+        return Tmp.c1.set(block.mapColor).mul(tnoise).a(1f - block.albedo);
+    }
+
+    @Override
+    public void genTile(Vec3 position, TileGen tile){
+        tile.floor = getBlock(position);
+        tile.block = tile.floor.asFloor().wall;
+
+        if(Ridged.noise3d(1, position.x, position.y, position.z, 2, 22) > 0.31){
+            tile.block = Blocks.air;
+        }
+    }
+    //Copied, just to test and see how it works - From BetaMindy
+    //TODO well, do my own code 
+    @Override
+    public void generateSector(Sector sector){
+
+        //these always have bases
+        if(sector.id == 154 || sector.id == 0){
+            sector.generateEnemyBase = true;
+            return;
+        }
+
+        PlanetGrid.Ptile tile = sector.tile;
+
+        boolean any = false;
+        float poles = Math.abs(tile.v.y);
+        float noise = Noise.snoise3(tile.v.x, tile.v.y, tile.v.z, 0.001f, 0.58f);
+
+        if(noise + poles/7.1 > 0.12 && poles > 0.23){
+            any = true;
+        }
+
+        if(noise < 0.16){
+            for(PlanetGrid.Ptile other : tile.tiles){
+                var osec = sector.planet.getSector(other);
+
+                //no sectors near start sector!
+                if(
+                        osec.id == sector.planet.startSector || //near starting sector
+                                osec.generateEnemyBase && poles < 0.85 || //near other base
+                                (sector.preset != null && noise < 0.11) //near preset
+                ){
+                    return;
+                }
+            }
+        }
+
+        sector.generateEnemyBase = any;
+    }
+
 }
