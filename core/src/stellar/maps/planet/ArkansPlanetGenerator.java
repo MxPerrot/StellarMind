a991b93b4aaff2e3a6870ef47547c1e5c4ba8ce4
Jojo <110781915+JojoFR1@users.noreply.github.com>
2022-08-20 00:55:17 +0200

    Update ArkansPlanetGenerator.java

--- core/src/stellar/maps/planet/ArkansPlanetGenerator.java
+++ core/src/stellar/maps/planet/ArkansPlanetGenerator.java
@@ -1,25 +1,31 @@
 package stellar.maps.planet;
 
+import static mindustry.Vars.*;
+
 import arc.graphics.*;
 import arc.math.*;
 import arc.math.geom.*;
-import arc.struct.*;
 import arc.util.*;
 import arc.util.noise.*;
 import mindustry.ai.*;
-import mindustry.ai.BaseRegistry.*;
 import mindustry.content.*;
 import mindustry.game.*;
-import mindustry.graphics.g3d.PlanetGrid.*;
 import mindustry.maps.generators.*;
 import mindustry.type.*;
 import mindustry.world.*;
 import mindustry.world.blocks.environment.*;
-import stellar.content.*;
-import stellar.world.blocks.envrionment.*;
+import mindustry.world.meta.*;
 
-import static mindustry.Vars.*;
+import stellar.content.*;
+import stellar.content.SBlocks;
+
 
 public class ArkansPlanetGenerator extends PlanetGenerator {
+
+    Block [] terrain = {Blocks.arkansFloor};
+
+    {
+        baseSeed = 3;
+    }
 
 }
