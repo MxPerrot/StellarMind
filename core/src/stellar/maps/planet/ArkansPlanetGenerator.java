662a280b700ebe5ee3ea293f60100a1bac6626a1
Jojo <110781915+JojoFR1@users.noreply.github.com>
2022-08-20 00:45:18 +0200

    Create ArkansPlanetGenerator.java

---
+++ core/src/stellar/maps/planet/ArkansPlanetGenerator.java
@@ -0,0 +1,25 @@
+package stellar.maps.planet;
+
+import arc.graphics.*;
+import arc.math.*;
+import arc.math.geom.*;
+import arc.struct.*;
+import arc.util.*;
+import arc.util.noise.*;
+import mindustry.ai.*;
+import mindustry.ai.BaseRegistry.*;
+import mindustry.content.*;
+import mindustry.game.*;
+import mindustry.graphics.g3d.PlanetGrid.*;
+import mindustry.maps.generators.*;
+import mindustry.type.*;
+import mindustry.world.*;
+import mindustry.world.blocks.environment.*;
+import stellar.content.*;
+import stellar.world.blocks.envrionment.*;
+
+import static mindustry.Vars.*;
+
+public class ArkansPlanetGenerator extends PlanetGenerator {
+
+}
