6e2eef07f4d6274ade0b3b6fb5ee24bf69050864
Jojo <110781915+JojoFR1@users.noreply.github.com>
2022-08-21 22:01:43 +0200

    Update StellarMind.java

--- core/src/stellar/StellarMind.java
+++ core/src/stellar/StellarMind.java
@@ -12,7 +12,7 @@ public class StellarMind extends Mod{
     }
     @Override
     public void loadContent(){
-        new StellarPlanets.load();
-        new StellarBlocks.load();
+        new StellarPlanets().load();
+        new StellarBlocks().load();
     }
 }
