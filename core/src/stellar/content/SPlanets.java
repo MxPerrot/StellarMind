b2ab8148c97fb8bf56a8668dd2e19353d1465152
Jojo <110781915+JojoFR1@users.noreply.github.com>
2022-08-21 13:42:14 +0200

    Typooo

--- core/src/stellar/content/SPlanets.java
+++ core/src/stellar/content/SPlanets.java
@@ -42,7 +42,7 @@ public class SPlanets {
         arkans = new Planet("arkans", elios, 1f, 2) {{
             generator = new ArkansPlanetGenerator();
             // Atmosphere
-            hasAmosphere = true;
+            hasAtmosphere = true;
             atmosphereColor = Color.valueOf("363f9a");
             atmosphereRadIn = 0.02f;
             atmosphereRadOut = 0.3f;
