d6e4f3b0993454a147e31b71337545de18b5a4c6
Jojo <110781915+JojoFR1@users.noreply.github.com>
2022-08-22 00:24:39 +0200

    Full rework

--- core/src/stellar/maps/planet/ArkansPlanetGenerator.java
+++ core/src/stellar/maps/planet/ArkansPlanetGenerator.java
@@ -1,56 +1,69 @@
 package stellar.maps.planet;
 
-import static mindustry.Vars.*;
-
-import arc.graphics.*;
 import arc.math.*;
-import arc.math.geom.*;
-import arc.struct.*;
 import arc.util.*;
+import arc.struct.*;
+import arc.graphics.*;
+import arc.math.geom.*;
 import arc.util.noise.*;
 import mindustry.ai.*;
-import mindustry.content.*;
 import mindustry.game.*;
-import mindustry.graphics.g3d.*;
-import mindustry.maps.generators.*;
 import mindustry.type.*;
 import mindustry.world.*;
+import mindustry.content.*;
+import mindustry.maps.planet.*;
+import mindustry.ai.BaseRegistry.*;
+import mindustry.maps.generators.*;
+import mindustry.graphics.g3d.PlanetGrid.*;
 import mindustry.world.blocks.environment.*;
-
-
 import stellar.content.*;
-import stellar.content.StellarBlocks.*;
 
+import static mindustry.Vars.*;
 
 public class ArkansPlanetGenerator extends PlanetGenerator {
 
-    Block[] terrain = {StellarBlocks.floorArkans, Blocks.water};
+   public static final int seed = 1f;
 
-    float scl = 2.7f;
+    public Block[][] arr =
+    {
+    {StellarEnvironment.floorArkans}
+    };
 
-    public float heightScl = 0.9f, octaves = 8, persistence = 0.7f, heightPow = 3f, heightMult = 1.6f;
+    ObjectMap<Block, Block> dec = ObjectMap.of(
+    Blocks.sporeMoss, Blocks.sporeCluster,
+    Blocks.moss, Blocks.sporeCluster,
+    Blocks.taintedWater, Blocks.water,
+    Blocks.darksandTaintedWater, Blocks.darksandWater
+    );
+
+    ObjectMap<Block, Block> tars = ObjectMap.of(
+    Blocks.sporeMoss, Blocks.shale,
+    Blocks.moss, Blocks.shale
+    );
+
+    float water = 0;
 
     float rawHeight(Vec3 position){
-        return Simplex.noise3d(seed, octaves, persistence, 1f/heightScl, 10f + position.x, 10f + position.y, 10f + position.z);
-    }
-
-    float rawTemp(Vec3 position){
-        return position.dst(0, 0, 1)*2.2f - Simplex.noise3d(seed, 8, 0.54f, 1.4f, 10f + position.x, 10f + position.y, 10f + position.z) * 2.9f;
+        position = Tmp.v33.set(position).scl(scl);
+        return (Mathf.pow(Simplex.noise3d(seed, 7, 0.5f, 1f/3f, position.x, position.y, position.z), 2.3f) + waterOffset) / (1f + waterOffset);
     }
 
     @Override
-    public float getHeight(Vec3 position){
-        return Mathf.pow(rawHeight(position), heightPow) * heightMult;
+    public void generateSector(Sector, sector){
+
+    }
+
+    @Override
+    public void getHeight(Vec3 position){
+        float height = rawHeight(position);
+        return Math.max(height, water);
     }
 
     @Override
     public Color getColor(Vec3 position){
         Block block = getBlock(position);
-        float tnoise = 1f;
-
-        if(block == StellarBlocks.floorArkans) block = StellarBlocks.floorArkans;
-
-        return Tmp.c1.set(block.mapColor).mul(tnoise).a(1f - block.albedo);
+        if(block == Blocks.salt) return Blocks.sand.mapColor;
+        return Tmp.c1.set(block.mapColor).a(1f - block.albedo);
     }
 
     @Override
@@ -58,47 +71,9 @@ public class ArkansPlanetGenerator extends PlanetGenerator {
         tile.floor = getBlock(position);
         tile.block = tile.floor.asFloor().wall;
 
-        if(Ridged.noise3d(1, position.x, position.y, position.z, 2, 22) > 0.31){
+        if(Riged.noise3d(1, position.x, position.y, position.z, 2, 22) > 0.31f){
             tile.block = Blocks.air;
         }
-    }
-    //Copied, just to test and see how it works - From BetaMindy
-    //TODO well, do my own code 
-    @Override
-    public void generateSector(Sector sector){
-
-        //these always have bases
-        if(sector.id == 154 || sector.id == 0){
-            sector.generateEnemyBase = true;
-            return;
-        }
-
-        PlanetGrid.Ptile tile = sector.tile;
-
-        boolean any = false;
-        float poles = Math.abs(tile.v.y);
-        float noise = Noise.snoise3(tile.v.x, tile.v.y, tile.v.z, 0.001f, 0.58f);
-
-        if(noise + poles/7.1 > 0.12 && poles > 0.23){
-            any = true;
-        }
-
-        if(noise < 0.16){
-            for(PlanetGrid.Ptile other : tile.tiles){
-                var osec = sector.planet.getSector(other);
-
-                //no sectors near start sector!
-                if(
-                        osec.id == sector.planet.startSector || //near starting sector
-                                osec.generateEnemyBase && poles < 0.85 || //near other base
-                                (sector.preset != null && noise < 0.11) //near preset
-                ){
-                    return;
-                }
-            }
-        }
-
-        sector.generateEnemyBase = any;
     }
 
     Block getBlock(Vec3 position){
@@ -107,10 +82,20 @@ public class ArkansPlanetGenerator extends PlanetGenerator {
         position = Tmp.v33.set(position).scl(scl);
         float rad = scl;
         float temp = Mathf.clamp(Math.abs(position.y * 2f) / (rad));
+        float tnoise = Simplex.noise3d(seed, 4, 0.56, 1f/3f, position.x, position.y + 999f, position.z);
+        temp = Mathf.lerp(temp, tnoise, 0.5f);
+	height *= 1.2f;
+	height = Mathf.clamp(height);
 
-        Block result = StellarBlocks.floorArkans;
+       float tar = Simplex.noise3d(seed, 4, 0.55f, 1f/2f, position.x, position.y + 999f, position.z) * 0.3f + Tmp.v31.dst(0, 0, 1f) * 0.2f;
 
-        return result;
+       Block res = arr[Mathf.clamp((int)(temp * arr.length), 0, arr[0].length - 1)][Mathf.clamp((int)(height * arr[0].length), 0, arr[0].length - 1)];
+           return res;
     }
 
+    @Override
+    public float noise(float x, float y, double octaves, double falloff, double scl, double mag){
+        Vec3 v = sector.rect.project(x, y).scl(5f);
+        return Simplex.noise3d(seed, octaves, falloff, 1f / scl, v.x, v.y, v.z) * (float)mag;
+    }
 }
