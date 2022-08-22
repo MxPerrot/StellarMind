4b538aa7a6f5434bfd0f54ea25a5945ebf1f22c5
Jojo <110781915+JojoFR1@users.noreply.github.com>
2022-08-23 00:07:15 +0200

    Update ArkansPlanetGenerator.java

--- core/src/stellar/maps/planet/ArkansPlanetGenerator.java
+++ core/src/stellar/maps/planet/ArkansPlanetGenerator.java
@@ -28,49 +28,35 @@ public class ArkansPlanetGenerator extends PlanetGenerator {
     float waterOffset = 0.07f;
     boolean genLakes = false;
 
-    public Block[][] arr =
-    {
-    {StellarEnvironment.floorArkans}
+    public Block[] arr = {
+        StellarEnvironment.floorArkans
     };
-
-    //ObjectMap<Block, Block> dec = ObjectMap.of(
-        //Blocks.sporeMoss, Blocks.sporeCluster,
-        //Blocks.moss, Blocks.sporeCluster,
-        //Blocks.taintedWater, Blocks.water,
-        //Blocks.darksandTaintedWater, Blocks.darksandWater
-    //);
-
-    //ObjectMap<Block, Block> tars = ObjectMap.of(
-        //Blocks.sporeMoss, Blocks.shale,
-        //Blocks.moss, Blocks.shale
-    //);
 
     float water = 0;
 
-    float rawHeight(Vec3 position){
+    float rawHeight(Vec3 position) {
         position = Tmp.v33.set(position).scl(scl);
         return (Mathf.pow(Simplex.noise3d(seed, 7, 0.5f, 1f/3f, position.x, position.y, position.z), 2.3f) + waterOffset) / (1f + waterOffset);
     }
 
     @Override
-    public void generateSector(Sector sector){
+    public void generateSector(Sector sector) {
 
     }
 
     @Override
-    public float getHeight(Vec3 position){
+    public float getHeight(Vec3 position) {
         float height = rawHeight(position);
         return Math.max(height, water);
     }
 
     @Override
-    public Color getColor(Vec3 position){
-        Block block = getBlock(position);
-        return Tmp.c1.set(block.mapColor).a(1f - block.albedo);
+    public Color getColor(Vec3 position) {
+        getBlock(position).mapColor;
     }
 
     @Override
-    public void genTile(Vec3 position, TileGen tile){
+    public void genTile(Vec3 position, TileGen tile) {
         tile.floor = getBlock(position);
         tile.block = tile.floor.asFloor().wall;
 
@@ -79,7 +65,7 @@ public class ArkansPlanetGenerator extends PlanetGenerator {
         }
     }
 
-    Block getBlock(Vec3 position){
+    Block getBlock(Vec3 position) {
         float height = rawHeight(position);
         Tmp.v31.set(position);
         position = Tmp.v33.set(position).scl(scl);
@@ -96,7 +82,7 @@ public class ArkansPlanetGenerator extends PlanetGenerator {
     }
 
     @Override
-    protected float noise(float x, float y, double octaves, double falloff, double scl, double mag){
+    protected float noise(float x, float y, double octaves, double falloff, double scl, double mag) {
         Vec3 v = sector.rect.project(x, y).scl(5f);
         return Simplex.noise3d(seed, octaves, falloff, 1f / scl, v.x, v.y, v.z) * (float)mag;
     }
