dd3952837b45367af84d36bb1804d2d282ef4617
Jojo <110781915+JojoFR1@users.noreply.github.com>
2022-08-23 14:56:24 +0200

    Update StellarEnvironment.java

--- core/src/stellar/content/StellarEnvironment.java
+++ core/src/stellar/content/StellarEnvironment.java
@@ -8,12 +8,12 @@ import mindustry.world.blocks.environment.*;
 
 public class StellarEnvironment {
     public static Block
-        floorArkans;
+        test;
 
     public void load() {
 
         //Arkans
-        floorArkans = new Floor("floor-arkans") {{
+        test = new Floor("test") {{
             attributes.set(Attribute.water, -0.25f);
             mapColor = Color.valueOf("ad190e");
         }};
