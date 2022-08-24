6d391f8018c73750f25ad2052177a39e79677cce
Jojo <110781915+JojoFR1@users.noreply.github.com>
2022-08-24 03:45:46 +0200

    update StellarResources.java

--- core/src/stellar/content/StellarResources.java
+++ core/src/stellar/content/StellarResources.java
@@ -1,2 +1,22 @@
 package stellar.content;
 
+import arc.struct.*;
+import arc.graphics.*;
+import mindustry.type.*;
+
+import static mindustry.content.Items.*;
+
+public class StellarResources {
+    public static Item aurase;
+
+    //public static Liquid
+
+    public static Seq<Item> arkansItems = new Seq<>();
+	public static Seq<Liquid> arkansLiquids = new Seq<>();
+	
+	public void load() {
+	    aurase = new Item("aurase", Color.valueOf("007fff")) {{cost: 2f;}};
+	    
+	    arkansItems.addAll(aurase, sand);
+	}
+}
