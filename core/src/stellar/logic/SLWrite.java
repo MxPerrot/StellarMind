244669eaa5d8e180a4ea39a074369a2e1257ade7
Jojo <110781915+JojoFR1@users.noreply.github.com>
2022-08-24 23:03:07 +0200

    Create SLWrite.java

---
+++ core/src/stellar/logic/SLWrite.java
@@ -0,0 +1,14 @@
+package stellar.logic;
+
+import arc.audio.*; 
+import arc.math.geom.*; 
+import mindustry.ai.types.*; 
+import mindustry.core.*; 
+import mindustry.entities.*; 
+import mindustry.gen.*; 
+import mindustry.logic.*; 
+import mindustry.logic.LExecutor.*;
+
+public class SLWrite {
+
+} 
